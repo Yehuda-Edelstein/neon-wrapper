2fe7f4dcae614b830f373700e05eee02d971bedd
Yehuda-Edelstein <yeauthor@gmail.com>
2024-08-16 11:48:04 -0400

    init

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Neon.css";
+
+const Neon = ({
+  color = "#ffee00",
+  glowIntensity = 10, // Glow intensity as a percentage
+  shadowSpread = 60, // Shadow spread as a percentage
+  noFlicker = false,
+  children,
+}) => {
+  return (
+    <div
+      className={`neon-text ${!noFlicker ? "flicker" : ""}`}
+      style={{
+        "--neon-color": color,
+        "--glow-intensity": `${glowIntensity}%`,
+        "--shadow-spread": `${shadowSpread}%`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Neon;
